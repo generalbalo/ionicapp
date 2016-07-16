22a61df3ece5e7652cd058161242d7ff51539b08
Balogun Wahab <dedomil22@gmail.com>
2016-07-16 23:37:24 +0100

    finished

--- www/js/app.js
+++ www/js/app.js
@@ -22,3 +22,121 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+//Config
+.config(function($stateProvider,$urlRouterProvider){
+  $stateProvider
+  .state('tabs',{
+    url:'/tab',
+    abstarct:true,
+    templateUrl:'templates/tabs.html'
+  })
+
+//The home route which is also the default route
+  .state('tabs.home',{
+    url:'/home',
+    views:{
+      'home-tab':{
+        templateUrl:'templates/home.html'
+      }
+    }
+  })
+
+//The list route which is a childof the tab route
+ .state('tabs.list',{
+    url:'/list',
+    views:{
+      'list-tab':{
+        templateUrl:'templates/list.html',
+        controller:'ListController'
+      }
+    }
+  })
+
+//Artist details Page which passes a parameter of artist id
+ .state('tabs.detail',{
+    url:'/list/:aId',
+    views:{
+      'list-tab':{
+        templateUrl:'templates/detail.html',
+        controller:'ListController'
+      }
+    }
+  })
+
+.state('tabs.calendar',{
+    url:'/calendar',
+    views:{
+      'calendar-tab':{
+        templateUrl:'templates/calendar.html',
+        controller:'CalendarController'
+      }
+    }
+  })
+
+//The default url the app goes to once loaded or refreshed
+$urlRouterProvider.otherwise('/tab/home');
+})
+
+.controller('CalendarController',['$scope','$http','$state',function($scope,$http,$state){
+  $http.get('js/data.json').success(function(data){
+    $scope.calendar=data.calendar;
+    
+//moveItem function enables the handling of item ordering
+    $scope.moveItem = function (item,fromIndex,toIndex) {
+      $scope.artists.splice(fromIndex,1);
+      $scope.artists.splice(toIndex,0,item); 
+    }
+
+//Remove Item from array function
+    $scope.removeItem = function(dayIndex,item){
+      $scope.calendar[dayIndex].schedule.splice($scope.calendar[dayIndex].indexOf(item),1);
+    }
+
+//Toggle the star
+    $scope.toggleStar = function(item){
+      item.star = !item.star;
+    }
+
+//Do refresh
+  	$scope.doRefresh = function() {
+      $http.get('js/data.json').success(function(data){
+      $scope.calendar=data.calendar;
+      $scope.$broadcast('scroll.refreshComplete');
+      });
+    }
+    })
+}])
+
+.controller('ListController',['$scope','$http','$state',function($scope,$http,$state){
+  $http.get('js/data.json').success(function(data){
+    $scope.artists=data.artists;
+    //Detail page parameter, send the paramter so the item can be filtered by that date
+    $scope.whichartist=$state.params.aId;
+
+//moveItem function enables the handling of item ordering
+    $scope.moveItem = function (item,fromIndex,toIndex) {
+      $scope.artists.splice(fromIndex,1);
+      $scope.artists.splice(toIndex,0,item); 
+    }
+
+//Remove Item from array function
+    $scope.removeItem = function(fromIndex){
+      $scope.artists.splice(fromIndex,1);
+    }
+
+//Toggle the star
+    $scope.toggleStar = function(item){
+      item.star = !item.star;
+    }
+
+//Do refresh
+  	$scope.doRefresh = function() {
+      $http.get('js/data.json').success(function(data){
+      $scope.artists=data;
+      $scope.$broadcast('scroll.refreshComplete');
+      });
+    }
+
+    });
+}])
